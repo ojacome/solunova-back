6f83d8f96b925c9807dc38d35ed55e0f8ec4574f
ojacome01 <ojacome@libelulasoft.com>
2021-05-13 22:23:30 -0500

    login

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,7 +1,7 @@
 const { response, request } = require('express');
 const userModel = require('../models/user.model');
 const bcrypt = require('bcryptjs');
-
+const {generarJWT} = require('../helpers/jwt.helper')
 
 
 const register = async(req = request, res = response) => {        
@@ -40,11 +40,44 @@ const register = async(req = request, res = response) => {
 
 }
 
-const login = (req = request, res = response) => {
-    res.status(500).json({
-        ok: false,
-        msg: 'llega'
-    });
+const login = async (req = request, res = response) => {
+    try {        
+        const { email, password } = req.body;
+
+        //verificar si existe usario registrado
+        const userDB = await userModel.findOne({ email });        
+        if ( !userDB ) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'El correo no se encuentra registrado'
+            });
+        }        
+        
+        // Verificar contraseña
+        const validPassword = bcrypt.compareSync( password, userDB.password );
+        if ( !validPassword ) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Las credenciales no son correctas'
+            });
+        }
+        
+        // Generar el TOKEN - JWT
+        const token = await generarJWT( userDB.id );
+
+        res.status(200).json({
+            ok: true,
+            token        
+        });
+
+    } catch (error) {
+        console.log(error)
+
+        res.status(500).json({
+            ok: false,
+            msg: 'Error al registrar usuario.'
+        });
+    }
 }
 
 
